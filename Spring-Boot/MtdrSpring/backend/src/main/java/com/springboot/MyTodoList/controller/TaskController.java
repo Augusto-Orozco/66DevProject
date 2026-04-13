94237c4160427aa85b058ff2db84c7e08b2a6299
Dario Puentes <a00838287@tec.mx>
2026-04-13 11:21:58 -0600

    Mostrar Tareas (Basico)

---
+++ Spring-Boot/MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/controller/TaskController.java
@@ -0,0 +1,25 @@
+package com.springboot.MyTodoList.controller;
+
+import com.springboot.MyTodoList.model.Task;
+import com.springboot.MyTodoList.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping(value = "/tasks")
+    public List<Task> getTasks() {
+        return taskService.getAllTasks();
+    }
+}
